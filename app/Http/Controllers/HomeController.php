5e895cd4dd09fd9d718694622938701ae8e41188
Vladimir Kabanets <karolsrebrny@gmail.com>
2021-03-08 15:02:09 +0200

    Commit 1

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Post;
+
+class HomeController
+{
+    public function __invoke()
+    {
+        $posts = Post::paginate(15);
+
+        return view('pages.posts', compact('posts'));
+    }
+
+}
